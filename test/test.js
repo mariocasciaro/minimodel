f9add2116d81a2397fb45273ec85256381893305
Mario Casciaro <mariocasciaro@gmail.com>
2013-10-21 20:36:18 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -302,13 +302,13 @@ describe('Date field', function() {
   it('should set date from string', function() {
     var post = new Post({date: "1382354005"});
     expect(post.date).to.be.instanceof(Date);
-    expect(post.date.toString()).to.be.equal("Fri Jan 16 1970 23:59:14 GMT+0000 (GMT)");
+    expect(post.date.toString()).to.contain("Fri Jan 16 1970 23:59:14 GMT+0000");
   });
   
   it('should set date from number', function() {
     var post = new Post({date: 1382354005});
     expect(post.date).to.be.instanceof(Date);
-    expect(post.date.toString()).to.be.equal("Fri Jan 16 1970 23:59:14 GMT+0000 (GMT)");
+    expect(post.date.toString()).to.contain("Fri Jan 16 1970 23:59:14 GMT+0000");
   });
 });
 
